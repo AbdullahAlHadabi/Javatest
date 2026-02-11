7578e5999f3a992f0e77a8392434be92f8286ff4
Abdullah <abdullah@email.com>
2026-02-11 09:15:35 +0400

    New

---
+++ Javatest/src/TheWeeklyHeader.java
@@ -0,0 +1,14 @@
+public class TheWeeklyHeader {
+
+    public static void main(String[] args) {
+
+        System.out.println("=".repeat(30));
+        System.out.println("Mon\tTue\tWed\tThuFri\tSat\tSun");
+        System.out.println("=".repeat(30));
+
+    }
+}
+
+
+
+
